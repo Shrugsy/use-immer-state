65054272704e2a5d06b183bb969ed63c354a2ce0
Shrugsy <joshfraser91@gmail.com>
2021-01-14 00:02:25 +1100

    Add state history + checkpoint features

--- src/index.test.ts
+++ src/index.test.ts
@@ -92,6 +92,25 @@ describe("useImmerState", () => {
       { id: 1, value: "newBar" },
       { id: 2, value: "faz" },
     ]);
+
+    // whole history should line up
+    const [, , { history }] = result.current;
+    expect(history).toEqual([
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "bar" },
+      ],
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "bar" },
+        { id: 2, value: "faz" },
+      ],
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "newBar" },
+        { id: 2, value: "faz" },
+      ],
+    ]);
   });
 
   test("logs and throws if mutations are detected", () => {
@@ -100,16 +119,407 @@ describe("useImmerState", () => {
       { id: 1, value: "bar" },
     ];
 
-    const { result, rerender } = renderHook(() => useImmerState(initialState));
+    const { rerender } = renderHook(() => useImmerState(initialState));
 
     // first render
-    let [state, setState] = result.current;
     initialState[0].value = "mutatedFoo";
 
     // silence the expected errors
     const mock = jest.spyOn(console, "error");
-    mock.mockImplementation(() => {});
+    mock.mockImplementation(() => null);
     expect(rerender).toThrow(MutationError);
     mock.mockRestore();
+  });
+
+  test("shows history, step, and can goto a point in history", () => {
+    const initialState = [
+      { id: 0, value: "foo" },
+      { id: 1, value: "bar" },
+    ];
+
+    const { result } = renderHook(() => useImmerState(initialState));
+
+    // first render
+    let [state, setState, { history, stepNum, goTo }] = result.current;
+    expect(stepNum).toEqual(0);
+    act(() => {
+      setState((prev) => {
+        prev[1].value = "newBar";
+      });
+    });
+
+    // second render
+    [state, setState, { history, stepNum, goTo }] = result.current;
+    expect(stepNum).toEqual(1);
+    expect(state).toEqual([
+      { id: 0, value: "foo" },
+      { id: 1, value: "newBar" },
+    ]);
+
+    // history should be extended
+    expect(history).toEqual([
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "bar" },
+      ],
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "newBar" },
+      ],
+    ]);
+
+    // set new state
+    act(() => {
+      setState((prev) => {
+        prev[0].value = "newFoo";
+      });
+    });
+
+    // third render
+    [state, setState, { history, stepNum, goTo }] = result.current;
+    expect(stepNum).toEqual(2);
+    expect(state).toEqual([
+      { id: 0, value: "newFoo" },
+      { id: 1, value: "newBar" },
+    ]);
+
+    // history should be extended
+    expect(history).toEqual([
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "bar" },
+      ],
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "newBar" },
+      ],
+      [
+        { id: 0, value: "newFoo" },
+        { id: 1, value: "newBar" },
+      ],
+    ]);
+
+    // set new state
+    act(() => {
+      setState((prev) => {
+        prev[0].value = "newNewFoo";
+      });
+    });
+
+    // fourth render
+    [state, setState, { history, stepNum, goTo }] = result.current;
+    expect(stepNum).toEqual(3);
+    expect(state).toEqual([
+      { id: 0, value: "newNewFoo" },
+      { id: 1, value: "newBar" },
+    ]);
+
+    // history should be extended
+    expect(history).toEqual([
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "bar" },
+      ],
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "newBar" },
+      ],
+      [
+        { id: 0, value: "newFoo" },
+        { id: 1, value: "newBar" },
+      ],
+      [
+        { id: 0, value: "newNewFoo" },
+        { id: 1, value: "newBar" },
+      ],
+    ]);
+    // time travel to step 1
+    act(() => {
+      goTo(1);
+    });
+
+    // fifth render
+    [state, setState, { history, stepNum, goTo }] = result.current;
+    expect(stepNum).toEqual(1);
+    expect(state).toEqual([
+      { id: 0, value: "foo" },
+      { id: 1, value: "newBar" },
+    ]);
+
+    // history should remain
+    expect(history).toEqual([
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "bar" },
+      ],
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "newBar" },
+      ],
+      [
+        { id: 0, value: "newFoo" },
+        { id: 1, value: "newBar" },
+      ],
+      [
+        { id: 0, value: "newNewFoo" },
+        { id: 1, value: "newBar" },
+      ],
+    ]);
+    // set new state, should purge any 'future' history and re-write new
+    act(() => {
+      setState((prev) => {
+        prev[1].value = "re-wrote the timeline";
+      });
+    });
+
+    // sixth render
+    [state, setState, { history, stepNum, goTo }] = result.current;
+
+    expect(stepNum).toEqual(2);
+    expect(state).toEqual([
+      { id: 0, value: "foo" },
+      { id: 1, value: "re-wrote the timeline" },
+    ]);
+
+    // history should be re-written
+    expect(history).toEqual([
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "bar" },
+      ],
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "newBar" },
+      ],
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "re-wrote the timeline" },
+      ],
+    ]);
+
+    // go to step 1
+    act(() => {
+      goTo(0);
+    });
+
+    // seventh render
+    [state, setState, { history, stepNum, goTo }] = result.current;
+
+    expect(stepNum).toEqual(0);
+    expect(state).toEqual([
+      { id: 0, value: "foo" },
+      { id: 1, value: "bar" },
+    ]);
+
+    // history should remain
+    expect(history).toEqual([
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "bar" },
+      ],
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "newBar" },
+      ],
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "re-wrote the timeline" },
+      ],
+    ]);
+
+    // set new state
+    // should work for non-function setter too
+    act(() => {
+      setState([
+        { id: 0, value: "faz" },
+        { id: 1, value: "baz" },
+        { id: 2, value: "hello" },
+        { id: 3, value: "world" },
+      ]);
+    });
+
+    // eighth render
+    [state, setState, { history, stepNum, goTo }] = result.current;
+
+    expect(stepNum).toEqual(1);
+    expect(state).toEqual([
+      { id: 0, value: "faz" },
+      { id: 1, value: "baz" },
+      { id: 2, value: "hello" },
+      { id: 3, value: "world" },
+    ]);
+
+    // history should be re-written
+    expect(history).toEqual([
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "bar" },
+      ],
+      [
+        { id: 0, value: "faz" },
+        { id: 1, value: "baz" },
+        { id: 2, value: "hello" },
+        { id: 3, value: "world" },
+      ],
+    ]);
+  });
+
+  test("can save a checkpoint", () => {
+    const initialState = [
+      { id: 0, value: "foo" },
+      { id: 1, value: "bar" },
+    ];
+
+    const { result } = renderHook(() => useImmerState(initialState));
+
+    // first render
+    let [
+      state,
+      setState,
+      { history, stepNum, saveCheckpoint, restoreCheckpoint },
+    ] = result.current;
+    expect(stepNum).toEqual(0);
+    act(() => {
+      setState((prev) => {
+        prev[1].value = "newBar";
+      });
+    });
+    act(() => {
+      setState((prev) => {
+        prev[0].value = "newFoo";
+      });
+    });
+    // [, , { saveCheckpoint }] = result.current;
+    [, , { saveCheckpoint }] = result.current;
+    act(() => {
+      saveCheckpoint();
+    });
+    act(() => {
+      setState([
+        { id: 0, value: "newNewFoo" },
+        { id: 1, value: "newNewBar" },
+        { id: 2, value: "faz" },
+      ]);
+    });
+    act(() => {
+      setState((prev) => {
+        prev[3] = { id: 3, value: "baz" };
+      });
+    });
+    // sixth render
+    [
+      state,
+      setState,
+      { history, stepNum, saveCheckpoint, restoreCheckpoint },
+    ] = result.current;
+    expect(stepNum).toEqual(4);
+    expect(state).toEqual([
+      { id: 0, value: "newNewFoo" },
+      { id: 1, value: "newNewBar" },
+      { id: 2, value: "faz" },
+      { id: 3, value: "baz" },
+    ]);
+    expect(history).toEqual([
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "bar" },
+      ],
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "newBar" },
+      ],
+      [
+        { id: 0, value: "newFoo" },
+        { id: 1, value: "newBar" },
+      ],
+      [
+        { id: 0, value: "newNewFoo" },
+        { id: 1, value: "newNewBar" },
+        { id: 2, value: "faz" },
+      ],
+      [
+        { id: 0, value: "newNewFoo" },
+        { id: 1, value: "newNewBar" },
+        { id: 2, value: "faz" },
+        { id: 3, value: "baz" },
+      ],
+    ]);
+
+    act(() => {
+      restoreCheckpoint();
+    });
+
+    // seventh render
+    [
+      state,
+      setState,
+      { history, stepNum, saveCheckpoint, restoreCheckpoint },
+    ] = result.current;
+    expect(stepNum).toEqual(2);
+    expect(state).toEqual([
+      { id: 0, value: "newFoo" },
+      { id: 1, value: "newBar" },
+    ]);
+    expect(history).toEqual([
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "bar" },
+      ],
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "newBar" },
+      ],
+      [
+        { id: 0, value: "newFoo" },
+        { id: 1, value: "newBar" },
+      ],
+      [
+        { id: 0, value: "newNewFoo" },
+        { id: 1, value: "newNewBar" },
+        { id: 2, value: "faz" },
+      ],
+      [
+        { id: 0, value: "newNewFoo" },
+        { id: 1, value: "newNewBar" },
+        { id: 2, value: "faz" },
+        { id: 3, value: "baz" },
+      ],
+    ]);
+
+    // setState now should override the future history
+    act(() => {
+      setState([
+        { id: 0, value: "clearedFoo" },
+        { id: 1, value: "clearedBar" },
+      ]);
+    });
+    [
+      state,
+      setState,
+      { history, stepNum, saveCheckpoint, restoreCheckpoint },
+    ] = result.current;
+    expect(stepNum).toEqual(3);
+    expect(state).toEqual([
+      { id: 0, value: "clearedFoo" },
+      { id: 1, value: "clearedBar" },
+    ]);
+    expect(history).toEqual([
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "bar" },
+      ],
+      [
+        { id: 0, value: "foo" },
+        { id: 1, value: "newBar" },
+      ],
+      [
+        { id: 0, value: "newFoo" },
+        { id: 1, value: "newBar" },
+      ],
+      [
+        { id: 0, value: "clearedFoo" },
+        { id: 1, value: "clearedBar" },
+      ],
+    ]);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -11,31 +11,110 @@ import { useTrackMutations } from "./utils";
  * @param initialState - initial state, or lazy function to return initial state
  */
 function useImmerState<S>(initialState: S | (() => S)) {
-  const [state, setState] = React.useState<S>(initialState);
+  let initialStateHistory: S[] | (() => S[]);
+  if (typeof initialState === "function") {
+    const passedInitialState = initialState as () => S;
+    initialStateHistory = () => [passedInitialState()];
+  } else {
+    initialStateHistory = [initialState];
+  }
+  const [stateHistory, setStateHistory] = React.useState<S[]>(
+    initialStateHistory
+  );
+  const stateHistoryRef = React.useRef(stateHistory);
+  stateHistoryRef.current = stateHistory;
+
+  const [stepNum, setStepNum] = React.useState(0);
+  const stepNumRef = React.useRef(stepNum);
+  stepNumRef.current = stepNum;
+
+  const [checkpoint, setCheckpoint] = React.useState(0);
 
   if (process.env.NODE_ENV !== "production") {
     // Yes we broke the rule, but kept the spirit.
     // The number of hooks won't change between renders,
     // because the environment won't change between renders.
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    useTrackMutations(state);
+    useTrackMutations(stateHistory);
   }
-
-  // TODO: allow saving a 'checkpoint'?
 
   const setStateWithImmer = React.useCallback(
     (updates: S | ((draftState: S) => void)) => {
       if (typeof updates === "function") {
-        const draftUpdates = updates as (draftState: S) => void;
-        setState((prevState) => produce(prevState, draftUpdates));
+        const draftUpdater = updates as (draftState: S) => void;
+        setStateHistory((prevStateHistory) => {
+          // chop any 'future' history that is no longer applicable
+
+          let newStateHistory = chopPastIndex(
+            prevStateHistory,
+            stepNumRef.current
+          );
+
+          // create the new history step based on the provided draft updater
+          const newStateItem = produce(
+            newStateHistory[stepNumRef.current],
+            draftUpdater
+          );
+          // add it as our new final step
+          newStateHistory = [...newStateHistory, newStateItem];
+
+          setStepNum(newStateHistory.length - 1);
+          return newStateHistory;
+        });
       } else {
-        setState(updates);
+        // chop any 'future' history that is no longer applicable
+        const nextStateHistory = chopPastIndex(
+          stateHistoryRef.current,
+          stepNumRef.current
+        );
+        setStepNum(nextStateHistory.length);
+        setStateHistory([...nextStateHistory, updates]);
       }
     },
     []
   );
 
-  return [state, setStateWithImmer] as const;
+  const goTo = React.useCallback((step: number) => {
+    if (typeof step !== "number") {
+      console.error(
+        `Please only pass a number to this function! Received ${step}`
+      );
+      return;
+    }
+    if (step < 0) {
+      console.error(`Step number ${step} below bounds!`);
+      return;
+    }
+    if (step > stateHistoryRef.current.length) {
+      console.error(
+        `Step number ${step} above bounds! History length is ${stateHistoryRef.current.length}`
+      );
+      return;
+    }
+    setStepNum(step);
+  }, []);
+
+  const saveCheckpoint = React.useCallback(() => {
+    setCheckpoint(stepNum);
+  }, [stepNum]);
+
+  const restoreCheckpoint = React.useCallback(() => {
+    goTo(checkpoint);
+  }, [goTo, checkpoint]);
+
+  const extraApi = {
+    history: stateHistory,
+    stepNum,
+    goTo,
+    saveCheckpoint,
+    restoreCheckpoint,
+  };
+
+  return [stateHistory[stepNum], setStateWithImmer, extraApi] as const;
 }
 
 export default useImmerState;
+
+function chopPastIndex<T>(arr: T[], index: number): T[] {
+  return arr.slice(0, index + 1);
+}
